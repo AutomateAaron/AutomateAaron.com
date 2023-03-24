5cd9977a196ba11fb6d8dd96e57f0530e74dd740
Aaron N. Brock <Contact@AaronNBrock.com>
2023-03-24 08:42:33 -0700

    header overhaul

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,9 +4,16 @@
   import Logo from "$lib/assets/images/logo.png";
   import Moon from "./svg/Moon.svelte";
   import Sun from "./svg/Sun.svelte";
+  import { onMount } from "svelte";
 
+  let outerElement;
+  let outerHeight;
+  let backgroundColorArr = [255, 255, 255];
+
+  let innerElement;
+  let innerHeight;
+  let heightDifference = 0;
   let scrollY = 0;
-
   let darkMode = false;
   let showMobileMenu = false;
 
@@ -49,124 +56,116 @@
   const handleOnScroll = () => {
     scrollY = window.scrollY;
   };
+
+  onMount(() => {
+    outerHeight = parseInt(window.getComputedStyle(outerElement).height, 10);
+    innerHeight = parseInt(window.getComputedStyle(innerElement).height, 10);
+    heightDifference = outerHeight - innerHeight;
+  });
 </script>
 
 <svelte:window on:scroll={handleOnScroll} />
-
-<header
-  class="duration-3 fixed left-5 right-5 top-4 z-50 rounded-lg transition-all ease-in-out lg:top-0 lg:left-0 lg:right-0 lg:rounded-none {scrollY >
-  100
+<!-- {scrollY > height_difference
     ? 'bg-opacity-100 shadow-lg bg-gray-100'
-    : 'bg-transparent lg:bg-opacity-0'}"
->
+    : 'bg-transparent lg:bg-opacity-0'} -->
+
+<header bind:this={outerElement} class="sticky -top-8 z-20 -mb-28 h-28">
   <div
-    class="ease-oet container flex items-center justify-between transition-all duration-300 {scrollY >
-    100
-      ? 'py-4 lg:py-5'
-      : 'py-4 lg:py-10'}"
+    class="mx-4 w-auto rounded-b-xl pt-8 lg:mx-0 {scrollY > heightDifference
+      ? 'shadow-lg bg-gray-100'
+      : 'bg-transparent'}"
   >
-    <a href="/">
-      <img src={Logo} alt="" class="h-10 w-10" />
-    </a>
-
-    <nav class="hidden lg:block">
-      <ul class="flex items-center justify-center">
-        {#each navItems as item}
-          <li>
-            <a
-              href={item.href}
-              class="hover:text-primary active:text-primary p-2 text-center text-lg transition-all duration-300 ease-in-out"
-              >{item.name}</a
-            >
-          </li>
-        {/each}
-      </ul>
-    </nav>
-
-    <div class="flex items-center justify-end space-x-6">
-      <button on:click={toggleDarkMode}>
-        {#if darkMode}
-          <Sun classNames="h-6 w-6 text-primary" />
-        {:else}
-          <Moon classNames="h-6 w-6 text-primary" />
-        {/if}
-      </button>
-
-      <a
-        href="/contact"
-        class="btn primary-btn hover:shadow-[0 8px 20px rgb(56 87 241 / 30%)] hidden hover:scale-105 lg:block"
-        >Get in Touch</a
-      >
-
-      <button
-        class="lg:hidden"
-        on:click={() => (showMobileMenu = !showMobileMenu)}
-      >
-        <span class="navbar-toggle-icon {showMobileMenu && 'icon-1'}" />
-        <span class="navbar-toggle-icon {showMobileMenu && 'opacity-0'}" />
-        <span class="navbar-toggle-icon {showMobileMenu && 'icon-3'}" />
-      </button>
-    </div>
-  </div>
-
-  <!-- mobile nav -->
-  {#if showMobileMenu}
     <div
-      transition:slide={{ duration: 400 }}
-      class="rounded-xl shadow-lg bg-gray-100 lg:hidden"
+      bind:this={innerElement}
+      class="container flex h-16 items-center justify-between lg:h-20"
     >
-      <nav class="px-5 py-4 ">
-        <ul class="space-y-2">
+      <a href="/">
+        <img src={Logo} alt="" class="h-10 w-10" />
+      </a>
+
+      <nav class="hidden lg:block">
+        <ul class="flex items-center justify-center">
           {#each navItems as item}
             <li>
               <a
                 href={item.href}
-                class=" hover:text-primary active:text-primary p-2 text-center text-lg transition-all duration-300 ease-in-out"
+                class="hover:text-primary active:text-primary p-2 text-center text-lg transition-all duration-300 ease-in-out"
                 >{item.name}</a
               >
             </li>
           {/each}
-
-          <li>
-            <a
-              href="/contact"
-              class="btn primary-btn hover:shadow-[0 8px 20px rgb(56 87 241 / 30%)] inline-block w-full hover:scale-105"
-              >Get in Touch</a
-            >
-          </li>
         </ul>
       </nav>
+
+      <div class="flex items-center justify-end space-x-6">
+        <button on:click={toggleDarkMode}>
+          {#if darkMode}
+            <Sun classNames="h-6 w-6 text-primary" />
+          {:else}
+            <Moon classNames="h-6 w-6 text-primary" />
+          {/if}
+        </button>
+
+        <a
+          href="/contact"
+          class="btn primary-btn hover:shadow-[0 8px 20px rgb(56 87 241 / 30%)] hidden hover:scale-105 lg:block"
+          >Get in Touch</a
+        >
+
+        <button
+          class="lg:hidden"
+          on:click={() => (showMobileMenu = !showMobileMenu)}
+        >
+          <span class="navbar-toggle-icon {showMobileMenu && 'icon-1'}" />
+          <span class="navbar-toggle-icon {showMobileMenu && 'opacity-0'}" />
+          <span class="navbar-toggle-icon {showMobileMenu && 'icon-3'}" />
+        </button>
+      </div>
     </div>
-  {/if}
+    <!-- mobile nav -->
+    {#if showMobileMenu}
+      <div
+        transition:slide={{ duration: 400 }}
+        class="rounded-xl shadow-lg bg-gray-100 lg:hidden"
+      >
+        <nav class="px-5 py-4 ">
+          <ul class="space-y-2">
+            {#each navItems as item}
+              <li>
+                <a
+                  on:click={() => (showMobileMenu = false)}
+                  href={item.href}
+                  class="hover:text-primary active:text-primary p-2 text-center text-lg transition-all duration-300 ease-in-out"
+                  >{item.name}</a
+                >
+              </li>
+            {/each}
+
+            <li>
+              <a
+                href="/contact"
+                class="btn primary-btn hover:shadow-[0 8px 20px rgb(56 87 241 / 30%)] inline-block w-full hover:scale-105"
+                >Get in Touch</a
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
+    {/if}
+  </div>
 </header>
 
 <style lang="postcss">
   :root {
-    --header-outer-height: 110px;
-    --header-inner-height: 70px;
+    --header-outer-height: theme("spacing.36");
+    --header-inner-height: theme("spacing.20");
     --header-height-difference: calc(
       var(--header-outer-height) - var(--header-inner-height)
     );
-    --header-bg: #fff;
-  }
-
-  body {
-    font-family: "DM Sans", sans-serif;
-    background-color: #f2f5f7;
-    line-height: 1.5;
-    min-height: 300vh;
-    position: relative;
-  }
-
-  .responsive-wrapper {
-    width: 90%;
-    max-width: 1280px;
-    margin-left: auto;
-    margin-right: auto;
   }
 
   /* Sticky header */
-  .header-outer {
+  #header-outer {
     /* Make it stick */
     height: var(--header-outer-height);
     position: sticky;
@@ -175,22 +174,14 @@
     ); /* Multiply by -1 to get a negative value */
     display: flex;
     align-items: center;
-
-    /* Other */
-    /* 	background-color: var(--header-bg); */
-    box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
+    margin-bottom: calc(var(--header-outer-height) * -1);
   }
 
-  .header-inner {
+  #header-inner {
     /* Make it stick */
     height: var(--header-inner-height);
     position: sticky;
     top: 0;
-
-    /* Other */
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
   }
 
   .navbar-toggle-icon {
