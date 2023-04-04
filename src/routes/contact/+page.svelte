7ad229675c332a5b24caf3c93895eeca65ffc8be
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-04 01:47:40 -0400

    netlify form test

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -184,16 +184,38 @@
           <button type="submit" class="btn-primary btn w-min">Send</button>
         </form> -->
 
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message" /></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form
+          name="netlify-form-example"
+          method="POST"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <input type="hidden" name="form-name" value="netlify-form-example" />
+          <label for="name">Name</label>
+          <input
+            name="name"
+            id="name"
+            required
+            placeholder="Name"
+            type="text"
+          />
+          <label for="email">Email</label>
+          <input
+            name="email"
+            id="email"
+            required
+            placeholder="Email"
+            type="email"
+          />
+          <label for="message">Message</label>
+          <input
+            name="message"
+            id="message"
+            required
+            placeholder="Message"
+            type="text"
+          />
+          <input type="submit" value="Submit" />
         </form>
       </div>
       <!-- image -->
