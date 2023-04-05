d4d340846413e00a6b86f63d249896162613ce5c
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-05 02:39:38 -0400

    netlify form test

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -296,7 +296,21 @@
                   on:click={() => {
                     message = undefined;
                   }}
-                  class="btn-ghost btn-sm btn">Deny</button
+                  class="btn-ghost btn-square btn-sm btn"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    ><path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    /></svg
+                  ></button
                 >
               </div>
             </div>
