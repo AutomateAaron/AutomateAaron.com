cc73f721e66227c4d10111e46a2957af05fdc32c
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-05 00:21:12 -0400

    netlify form test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,23 +13,11 @@
 
   import Header from "$lib/components/layout/Header.svelte";
   import Footer from "$lib/components/layout/Footer.svelte";
-  import { currentPage, isMenuOpen } from "$lib/assets/js/store";
   import { navItems, siteMap } from "$lib/config";
   import { preloadCode } from "$app/navigation";
   import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
   import TawkTo from "$lib/components/TawkTo.svelte";
-
-  export let data;
-
-  const transitionIn = { delay: 150, duration: 150 };
-  const transitionOut = { duration: 100 };
-
-  /**
-   * Updates the global store with the current path. (Used for highlighting
-   * the current page in the nav, but could be useful for other purposes.)
-   **/
-  $: currentPage.set(data.path);
+  import GoogleAnalytics from "$lib/components/GoogleAnalytics.svelte";
 
   /**
    * This pre-fetches all top-level routes on the site in the background for faster loading.
@@ -50,12 +38,13 @@
 	The below markup is used on every page in the site. The <slot> is where the page's
 	actual contents will show up.
 -->
-<div class="layout" class:open={$isMenuOpen}>
+<div class="layout">
   <Header />
-  <main id="main" tabindex="-1" in:fade={transitionIn} out:fade={transitionOut}>
+  <main id="main" tabindex="-1">
     <slot />
   </main>
   <Footer />
   <div id="teleport" />
   <TawkTo />
+  <GoogleAnalytics />
 </div>
