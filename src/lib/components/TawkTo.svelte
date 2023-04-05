eac4cc115f258d411dfa55633f6af85b83b89176
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-04 23:24:52 -0400

    netlify form test

---
+++ src/lib/components/TawkTo.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { teleport } from "$lib/assets/js/clientUtils";
+</script>
+
+<div use:teleport>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/642cb5e131ebfa0fe7f68676/1gt7acu92";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+</div>
