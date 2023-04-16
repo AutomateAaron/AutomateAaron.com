9b4279b91b65e3ab00d4d7046056b937a6056ca1
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-16 04:14:39 -0400

    netlify form test

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -212,7 +212,7 @@
         <h3 class="h3">Or, drop me a line here...</h3>
 
         <form
-          use:enhance
+          use:enhance={handleSubmit}
           enctype="application/x-www-form-urlencoded"
           name="contact"
           method="POST"
