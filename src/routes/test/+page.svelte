7704d60610e1ea53ecb771932c1f93372ee6cad9
Aaron N. Brock <contact@aaronnbrock.com>
2023-04-16 04:29:19 -0400

    netlify form test

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -3,4 +3,24 @@
   import EmailIcon from "~icons/ic/baseline-email";
 </script>
 
-<section />
+<section class="section flex h-screen items-center justify-center">
+  <div class="bg-base-100 p-8">
+    <form
+      name="netlify-form-example"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+    >
+      <input type="hidden" name="form-name" value="netlify-form-example" />
+      <label for="message">Message</label>
+      <input
+        name="message"
+        id="message"
+        required
+        placeholder="Message"
+        type="text"
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</section>
